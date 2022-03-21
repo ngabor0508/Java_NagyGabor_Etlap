05a1cc71ef3e977418bc04f1ba4cc3644faaa832
Nagy Gabor <ngabor0508@gmail.com>
2022-03-21 10:01:10 +0100

    Hiba kijavítva, "pom" fájlba nem volt felvéve a mysql dependency. :(

--- src/main/java/hu/nagy_gabor/nagygabor_etlap/Controllers/MainController.java
+++ src/main/java/hu/nagy_gabor/nagygabor_etlap/Controllers/MainController.java
@@ -7,17 +7,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainController extends Controller {
-
-    @FXML
-    private Button torles;
-    @FXML
-    private Button ujfelvetel;
-
     @FXML
     private TableView<Etlap> etlapTable;
     @FXML
@@ -27,11 +23,15 @@ public class MainController extends Controller {
     @FXML
     private TableColumn<Etlap, String> colKategoria;
     private EtlapDb db;
+    @FXML
+    private Spinner inputSzazalek;
+    @FXML
+    private Spinner inputFt;
 
     public void initialize(){
-        colNev.setCellValueFactory(new PropertyValueFactory<>("cim"));
-        colAr.setCellValueFactory(new PropertyValueFactory<>("cim"));
-        colKategoria.setCellValueFactory(new PropertyValueFactory<>("cim"));
+        colNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        colAr.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        colKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
 
         try {
             db = new EtlapDb();
@@ -39,18 +39,21 @@ public class MainController extends Controller {
         } catch (SQLException e) {
             System.out.println("HIBA!");
         }
+    }
 
+    public TableColumn<Etlap, String> getColNev() {
+        return colNev;
     }
 
     private void etlapListaFeltolt() {
         try {
             List<Etlap> etlapList = db.getEtlap();
             etlapTable.getItems().clear();
-            for(Etlap etlap:etlapList){
+            for(Etlap etlap: etlapList){
                 etlapTable.getItems().add(etlap);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Ez egy másik hiba.");
         }
     }
 
